003fa93db262eba5d8c2b46997dadcc17958a8ec
ファム　ディン　チョン <phamtrongdev@Macbook.local>
2022-03-09 18:54:03 +0900

    update views.py

--- phamtrongblog/views.py
+++ phamtrongblog/views.py
@@ -5,4 +5,5 @@ from django.template import loader
 # Create your views here.
 def index(request):
     template = loader.get_template('homepage/index.html')
-    return HttpResponse(template.render(None,request))
+    context = {'name':'Trong'}
+    return HttpResponse(template.render(context,request))
